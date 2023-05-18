a4f9d07f96618c6bb44b07822a8a9ed7c2615f81
Vlad Riscutia <vladris@outlook.com>
2023-05-18 10:19:27 +0200

    Simplify 3.24

--- code/03/24.py
+++ code/03/24.py
@@ -2,14 +2,13 @@ from llm_utils import ChatTemplate
 
 chat = ChatTemplate(
     {'messages': [{'role': 'system', 'content': 'You are an AI chat program.'},
-                  {'role': 'user', 'content': None}]})
+                  {'role': 'user', 'content': '{prompt}'}]})
 
 while True:
     prompt = input('user: ')
     if prompt == 'exit':
         break
 
-    chat.template['messages'][1]['content'] = prompt
     message = chat.completion({'prompt': prompt}).choices[0].message
 
     print(f'{message.role}: {message.content}')
