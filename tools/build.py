db2e72deb9c38db1e1f8a8d400f5b55ec2cd8726
Vlad Riscutia <vladris@outlook.com>
2023-06-01 12:23:45 +0300

    Implement hamburger menu

--- tools/build.py
+++ tools/build.py
@@ -18,6 +18,14 @@ chapter_titles = {
     '05': 'Memory and Embeddings',
 }
 
+# Chapter prefix mappings
+chapter_prefixes = {
+    '01': 'Chapter 1',
+    '02': 'Chapter 2',
+    '03': 'Chapter 3',
+    '04': 'Chapter 4',
+    '05': 'Chapter 5',
+}
 
 # Render markdown files
 md = make_markdown_processor()
@@ -26,7 +34,8 @@ for file in os.listdir('text'):
         continue
 
     filepath = os.path.join('text', file)
-    title = chapter_titles[file.split('.')[0]]
+    index = file.split('.')[0]
+    title, prefix = chapter_titles[index], chapter_prefixes[index]
     outpath = os.path.join('site', title.lower().replace(' ', '-') + '.html')
 
     print(f'Processing {filepath} -> {outpath}')
@@ -34,4 +43,4 @@ for file in os.listdir('text'):
         text = f.read()
 
     with open(outpath, 'w+') as f:
-        f.write(render(VerySimpleTemplate('./tools/template.html'), {'content': md(text), 'title': title}))
+        f.write(render(VerySimpleTemplate('./tools/template.html'), {'content': md(text), 'prefix': prefix, 'title': title}))
