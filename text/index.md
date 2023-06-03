b4d36fdc7fb4eb6ab600072e7199b46b9f39e5c0
Vlad Riscutia <vladris@outlook.com>
2023-06-03 10:13:51 +0200

    Update template with more links, add cover, toc

---
+++ text/index.md
@@ -0,0 +1,13 @@
+# Large Language Models at Work
+
+## Enhancing Software Systems with AI-Powered Language Models
+
+![Book cover](images/cover/cover.png)
+
+> *Giant language minds  
+> Transforming words and culture  
+> Limitless power*  
+
+Haiku by `gpt-3.5-turbo`
+
+Version `0.1`. Next: [Table of Contents](./table-of-contents.html)
--- tools/build.py
+++ tools/build.py
@@ -1,16 +1,22 @@
 import os
+import shutil
 from baku.markdown import make_markdown_processor
 from baku.templating import render, VerySimpleTemplate
 
 
 # Clean up the site directory
+print('Cleaning up site directory')
 for file in os.listdir('site'):
     if file.endswith('.html'):
         os.remove(os.path.join('site', file))
 
+print('Removing images')
+shutil.rmtree('site/images', ignore_errors=True)
 
 # Chapter title mappings
 chapter_titles = {
+    'index': 'Front Cover',
+    'toc': 'Table of Contents',
     '01': 'A New Paradigm',
     '02': 'Large Language Models',
     '03': 'Prompt Engineering',
@@ -20,11 +26,13 @@ chapter_titles = {
 
 # Chapter prefix mappings
 chapter_prefixes = {
-    '01': 'Chapter 1',
-    '02': 'Chapter 2',
-    '03': 'Chapter 3',
-    '04': 'Chapter 4',
-    '05': 'Chapter 5',
+    'index': '',
+    'toc': '',
+    '01': 'Chapter 1: ',
+    '02': 'Chapter 2: ',
+    '03': 'Chapter 3: ',
+    '04': 'Chapter 4: ',
+    '05': 'Chapter 5: ',
 }
 
 # Render markdown files
@@ -36,7 +44,11 @@ for file in os.listdir('text'):
     filepath = os.path.join('text', file)
     index = file.split('.')[0]
     title, prefix = chapter_titles[index], chapter_prefixes[index]
-    outpath = os.path.join('site', title.lower().replace(' ', '-') + '.html')
+    
+    if index != 'index':
+        outpath = os.path.join('site', title.lower().replace(' ', '-') + '.html')
+    else:
+        outpath = os.path.join('site', 'index.html')
 
     print(f'Processing {filepath} -> {outpath}')
     with open(filepath, 'r') as f:
@@ -44,3 +56,10 @@ for file in os.listdir('text'):
 
     with open(outpath, 'w+') as f:
         f.write(render(VerySimpleTemplate('./tools/template.html'), {'content': md(text), 'prefix': prefix, 'title': title}))
+
+
+# Copy the directory tree
+print('Copying images')
+shutil.copytree('text/images', 'site/images')
+
+print('Done')
