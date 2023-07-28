97e3cd078b793721e0925f7c7f40ee54156d09d0
Vlad Riscutia <vladris@outlook.com>
2023-07-28 16:29:01 -0700

    Update

--- code/07/01.py
+++ code/07/01.py
@@ -4,4 +4,3 @@ chat = ChatTemplate({
     'messages': [{'role': 'user', 'content': 'Write a short 3 chapter sci-fi novella on the theme of AI ethics.'}]})
 
 print(chat.completion({}).choices[0].message.content)
-
